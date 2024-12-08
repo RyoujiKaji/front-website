80a822555873db7f0c919107668901da5cca8dac
Bychkova <nadiajiayufon@gmail.com>
2024-12-08 22:10:33 +0300

    for post to githab

--- src/enter/components/EnterForm.js
+++ src/enter/components/EnterForm.js
@@ -32,7 +32,7 @@ const EnterForm = () => {
     }
 
     try {
-      /* const response = await fetch('https://example.com/api/login', { // Замените на Ваш API //await - без ответа действие не продолжится
+       const response = await fetch('http://localhost:8080/db/enter', { // Замените на Ваш API //await - без ответа действие не продолжится
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
@@ -44,12 +44,12 @@ const EnterForm = () => {
         throw new Error('Неверная почта или пароль');
       }
 
-      const data = await response.json(); //разобрать //получаем тело ответа в формате json объекта  */
-      let userData = {
+      const userData = await response.json(); //разобрать //получаем тело ответа в формате json объекта  
+      /* let userData = {
         id: 1,
         role: 'user'
       };
-
+ */
       setData({
         id: userData.id,
         role: userData.role
